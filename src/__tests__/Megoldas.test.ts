b1e3a1cbf903be8adf46d3fe9bd1bbbabd07cf48
[Erik Korsos] <[erik.korsos@gmail.com]>
2024-11-26 07:59:36 +0100

    3.feladat test complete

--- src/__tests__/Megoldas.test.ts
+++ src/__tests__/Megoldas.test.ts
@@ -2,10 +2,16 @@ import { Megoldas } from "../Megoldas";
 const instance: Megoldas = new Megoldas("autok.txt");
 
 describe("Megoldas osztály unit tesztek", () => {
-    it("Should be CEG300",() => {
+    // 2. Feladat
+    it("Should be CEG300", () => {
         expect(instance.getLastCarOut()?.RegNumber).toBe("CEG300");
-    })
-    it("Should be 4",() => {
+    });
+    // 3. Feladat
+    it("Sholud be 2 cars on day 4", () => {
+        expect(instance.getCarsOnGivenDay(4).length).toBe(2);
+    });
+    // 4. Feladat
+    it("Should be 4", () => {
         expect(instance.getNotArrivedCars()).toBe(4);
-    })
+    });
 });
